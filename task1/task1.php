a53fad3193d9ce5bd51279bfcff352acdbcfb8ce
Maksim Bondarenko <mbondarenko92@gmail.com>
2014-07-15 16:05:24 +0700

    Small debug

--- task1/task1.php
+++ task1/task1.php
@@ -3,6 +3,6 @@
     $ids = array(1, 2, 3, 4);
     
 
-    $sql = 'SELECT * FROM `table` WHERE `id` IN ('. implode (", ", $ids) . ');';
+    $sql = 'SELECT * FROM `table` WHERE `id` IN ('. implode (", ", $ids) . ');'; # Bit cooler than loops
     echo "$sql\n";
 ?>
--- task3/task3.php
+++ task3/task3.php
@@ -1,5 +1,6 @@
 <?php
 
-    echo date("d/m/Y") . "\n"; # What means "удобочитаемый"? I like this format
-
+    #echo date("d/m/Y") . "\n"; # What means "удобочитаемый"? I like this format
+    
+    echo date('l \t\h\e jS F, Y'). "\n"; # But this looks fuller
 ?>
